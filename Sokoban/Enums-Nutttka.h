6a6b269bdb8172a85c2ee9aaa1c06715c6ca73a9
Anna ☆ <aycods@gmail.com>
2021-10-29 08:43:30 +0300

    New vers

---
+++ Sokoban/Enums-Nutttka.h
@@ -0,0 +1,87 @@
+#pragma once
+enum KeyPress
+{
+	KEY_PRESS_DEFAULT,
+	KEY_PRESS_UP,
+	KEY_PRESS_DOWN,
+	KEY_PRESS_LEFT,
+	KEY_PRESS_RIGHT,
+	KEY_PRESS_LEFT_CTRL,
+	KEY_PRESS_Z
+};
+enum Object
+{
+	EMPTY,
+	WALL,
+	FLOOR,
+	CAT,
+	PRESENT,
+	BACKGROUND,
+	PLACEHERE,
+	BOX_IN_PLACEHERE, 
+	BACKGROUND_TIME
+};
+enum Buttons
+{
+	GAME,
+	HOW_TO_PLAY,
+	EXIT,
+	YES,
+	NO,
+	NEXT,
+	BACK
+};
+enum Wallps
+{
+	LOGO_OF_GROUP,
+	LOGO_OF_GAME,
+	MAIN_MENU,
+	MENU_EXIT,
+};
+enum CountOdArrs
+{
+	CountOfLogos = 4,
+	CountOfButtons = 7,
+	CountOfPressedButtons = CountOfButtons,
+	CountOfTexture = 9,
+	CountOfWindows = 5,
+	CountOfNumbers = 12,
+	CountOfLeveles = 6,
+	CountOfComic = 5
+	//CountOfWindowsHowToPlay = 5
+
+};
+enum SomeWindows
+{
+	FIRST,
+	SECOND,
+	THIRD,
+	FOURTH,
+	FIFTH,
+	WIN_WIND
+};
+enum ReturningValues
+{
+	EXIT_TO_MENU,
+	WIN,
+	RECURSION
+};
+enum Surfaces
+{
+    LOGOS,
+	BUTTONS,
+	PRESSED_BUTTONS,
+	TEXTURES,
+	WINDOWS, 
+	NUMBERS,
+	LEVELES,
+	COMIC
+};
+enum Music
+{
+	INTRO,
+	MENU,
+	SOUND_GAME,
+	BOX,
+	BUTTON
+};
